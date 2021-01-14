e7bbd19f86f9bf531eb4af2113bd2e1d3891301b
Will Andre <wandre94@gmail.com>
2021-01-14 21:16:45 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name
+  end
+
+  def initialize(breed, Mutt)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+end
